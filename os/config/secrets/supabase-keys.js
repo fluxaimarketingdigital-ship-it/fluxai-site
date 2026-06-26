c13c42bf43635d0343338d205bd10a9b940aae36
unknown <fluxai.marketingdigital@gmail.com>
2026-06-26 16:56:34 -0300

    chore(frontend): Correções de proxy Supabase, CSS Text-Balance e links sociais

--- os/config/secrets/supabase-keys.js
+++ os/config/secrets/supabase-keys.js
@@ -4,6 +4,6 @@
  */
 
 export const SUPABASE_CONFIG = {
-    url: "https://mufgwetfhfhhmhowbhjj.supabase.co",
+    url: "https://rmbxeikejzbcfiooylsd.supabase.co",
     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11Zmd3ZXRmaGZoaG1ob3diaGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1Mzg1MDYsImV4cCI6MjA5NDExNDUwNn0.G0VxvE6acPRKZIwee7d2ARBkIdqf9SRvVI1uagMrBZI"
 };
